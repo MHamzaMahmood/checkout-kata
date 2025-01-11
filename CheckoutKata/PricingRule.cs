4d972d22b4d69949ad69630a20447c0c2ca55b47
Hamza <hamzamahmood1308@gmail.com>
2025-01-11 23:38:31 +0000

    Add the PricingRules class

---
+++ CheckoutKata/PricingRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class PricingRule
+    {
+        public string SKU { get; }
+        public int UnitPrice { get; }
+        public int? SpecialQuantity { get; }
+        public int? SpecialPrice { get; }
+
+        public PricingRule(string sku, int unitPrice, int? specialQuantity = null, int? specialPrice = null) 
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialQuantity = specialQuantity;
+            SpecialPrice = specialPrice;
+        }
+    }
+}
