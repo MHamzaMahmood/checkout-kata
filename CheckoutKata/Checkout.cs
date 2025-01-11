5f00bbacc1e2d405c413e43356abb0b44fd3e125
Hamza <hamzamahmood1308@gmail.com>
2025-01-11 23:39:17 +0000

    Add a basic Checkout implementation
    To be built out shortly with some actual functionality for the first test to be able to pass

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Checkout : ICheckout
+    {
+        private readonly List<PricingRule> _pricingRules;
+
+        public Checkout(List<PricingRule> pricingRules)
+        {
+            _pricingRules = pricingRules;
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
