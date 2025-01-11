b8e43d9680da3b4f712dd5adc60f389155bfb20b
Hamza <hamzamahmood1308@gmail.com>
2025-01-11 23:18:27 +0000

    Add pricing rules to the test setup
    Created a set of pricing rules here for the tests in the following format: SKU, single unit price, qualifying quantity for the special offer, price for that special offer

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -2,9 +2,18 @@
 
 public class CheckoutTests
 {
+    private List<PricingRule> _pricingRules;
+
     [SetUp]
     public void Setup()
     {
+        _pricingRules = new List<PricingRule>
+        {
+            new PricingRule("A", 50, 3, 130),
+            new PricingRule("B", 30, 2, 45),
+            new PricingRule("C", 20),
+            new PricingRule("D", 15)
+        };
     }
 
     [Test]
