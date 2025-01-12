26c343620a36ace15bbcf42365a9b48cd490c50d
Hamza <hamzamahmood1308@gmail.com>
2025-01-12 16:56:10 +0000

    Add the test for multiple mixed items with multiple offers
    Will not pass - functionality not implemented as previous commits for tests state

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -94,4 +94,22 @@ public class CheckoutTests
         //Assert
         Assert.That(total, Is.EqualTo(165));
     }
+
+    [Test]
+    public void ScanMultipleMixedItemsWithMultipleOffers()
+    {
+        //Arrange
+        var checkout = new Checkout(_pricingRules);
+
+        //Act
+        checkout.Scan("A");
+        checkout.Scan("B");
+        checkout.Scan("A");
+        checkout.Scan("A");
+        checkout.Scan("B");
+        var total = checkout.GetTotalPrice();
+
+        //Assert
+        Assert.That(total, Is.EqualTo(175));
+    }
 }
