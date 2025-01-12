c06c82226658812a9a531ecce41ee957f6a14f65
Hamza <hamzamahmood1308@gmail.com>
2025-01-12 20:50:03 +0000

    Handle empty sku in the same way as invalid sku

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -167,10 +167,10 @@ public class CheckoutTests
     public void TestInvalidEmptySKU()
     {
         //Arrange
-        var emptySku = "";
+        var checkout = new Checkout(_pricingRules);
 
         //Act & Assert
-        var exception = Assert.Throws<ArgumentNullException>(() => new ScannedItem(emptySku));
+        var exception = Assert.Throws<ArgumentException>(() => checkout.Scan(""));
         Assert.That(exception.Message, Does.Contain("Please provide a valid SKU"));
     }
 
